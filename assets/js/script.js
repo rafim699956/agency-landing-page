16080c86b794392430074d4330aa2c602777fa04
rafim699956 <rafim699956@gmail.com>
2024-09-14 16:30:22 +0600

    header and body part end

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var splide = new Splide('.splide', {
+    perPage: 3,
+    rewind: true,
+    type: 'loop',
+    arrows: false,
+    gap: '1.875rem',
+    focus:'center',
+    padding:'0',
+});
+
+splide.mount();
