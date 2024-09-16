721f314b004d3ace7b30a65fd6fd13228c071d7e
rafim699956 <rafim699956@gmail.com>
2024-09-16 13:02:22 +0600

    body part style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var splide = new Splide('.splide', {
     perPage: 3,
     rewind: true,
-    type: 'loop',
     arrows: false,
     gap: '1.875rem',
-    focus:'center',
-    padding:'0',
+    focus: 'center',
+    padding: '0',
+    speed: 100,
+    type: 'loop',
 });
 
 splide.mount();
